fa54101e6d20f6c2f577f6e7aec13b23f978a950
Elco Klingen <disillusion84@gmail.com>
2026-02-20 12:05:04 +0100

    changed phone number

--- website/assets/scripts/utilities/basic-email-protection.js
+++ website/assets/scripts/utilities/basic-email-protection.js
@@ -2,12 +2,12 @@ export default function basicEmailProtection() {
   const elements = [...document.querySelectorAll('[href^="mailto:"], [href^="tel:"]')]
 
   elements.forEach(element => {
-    element.innerHTML = element.innerHTML.replace(/_send_me_an_/g, 'anna.nogar@gmail.com').replace(/_or_pick_up_the_/g, '+31&nbsp;6&nbsp;25315441')
+    element.innerHTML = element.innerHTML.replace(/_send_me_an_/g, 'anna.nogar@gmail.com').replace(/_or_pick_up_the_/g, '+39&nbsp;351&nbsp;395&nbsp;4531')
 
     if (element.hasAttribute('href')) {
       const href = element.getAttribute('href')
 
-      element.setAttribute('href', href.replace(/_send_me_an_/g, 'anna.nogar@gmail.com').replace(/_or_pick_up_the_/g, '+31625315441'))
+      element.setAttribute('href', href.replace(/_send_me_an_/g, 'anna.nogar@gmail.com').replace(/_or_pick_up_the_/g, '+393513954531'))
     }
   })
 }
